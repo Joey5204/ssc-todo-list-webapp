398f95d5bdfc38efb0c669b8896c72783f2df164
Joey5204 <yuehhsuan.wu@student.mahidol.edu>
2025-02-21 22:17:55 +0700

    second commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import io.muzoo.ssc.assignment.tracker.SscAssignment;
+
+public class Main extends SscAssignment{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
